21f7586cc308cbe009c42abd95f22fa95b81df23
Rony <ronal@RONY>
2026-06-10 00:46:00 -0600

    Ronaldo. Fecha: 10 de junio de 2026 Hora: 00:40 (12:40 a. m.)

---
+++ pryProyecto/ClaseConexion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySqlConnector;
+
+namespace pryProyecto
+{
+    internal class ClaseConexion
+    {
+        private string host = "189.240.192.140";
+        private string bd = "bdescuela3b";
+        private string usuario = "terceroB";
+        private string contraseña = "tercero@B";
+        private string puerto = "3306";
+
+        private string cadenaConexion => $"server = {host}; datebase = {bd}; user = {usuario}; password = {contraseña}; puerto = {puerto};";
+
+        public MySqlConnection AbrirConexion()
+        {
+            var conexion = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            } catch(Exception ex)
+            {
+                throw new Exception("Error al intentar conectar la base de datos" + ex.Message,ex);
+            }
+        }
+
+        public void CerrarConexion(MySqlConnection conexion)
+        {
+            try
+            {
+                if (conexion != null && conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                    conexion.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al cerrar la conexion con la base de datos" + ex.Message, ex);
+            }
+        }
+    }
+}
