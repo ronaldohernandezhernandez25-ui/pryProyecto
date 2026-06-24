0e323e702c3cc4e8b99ae864a03a2cfbb80fd9b8
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2026-06-24 08:22:25 -0600

    eliminar y cargar tabla

--- pryProyecto/clsTutor.cs
+++ pryProyecto/clsTutor.cs
@@ -30,7 +30,7 @@ namespace pryProyecto
 
 
 
-        
+
 
         //Metodo para consultar por coincidencias
         public DataTable Consultar()
@@ -126,8 +126,61 @@ namespace pryProyecto
             }
             return msg;
         }
+        public string Eliminar()
+        {
+            string msg = "";
+            try
+            {
+                ClaseConexion conexionBD = new ClaseConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "DELETE FROM tbltutores C WHERE C.idTutor = @idtutor;";
+                    using (comando = new MySqlCommand(sql, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@idTutor ", IdTutor);
+                        int filasAfectadas = comando.ExecuteNonQuery();
+                        if (filasAfectadas > 0)
+                        {
+                            msg = "Datos eliminados correctamente";
+                        }
+                        else
+                        {
+                            msg = "Los datos no se pudieron eliminar";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error" + ex.Message);
+            }
+            return msg;
+        }
 
-       
+        internal object? cargarTabla()
+        {
+            tabla = new DataTable();
+            try
+            {
+                ClaseConexion conexionBD = new ClaseConexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT idTutor AS Clave, nombreTutor AS Tutor, " + "parentesco AS Parentesco, direccion AS Direccion, " + "telefono AS Telefono, correo AS Correo " + "FROM tbltutores;"; ;
+                    using (consulta = new MySqlDataAdapter(sql, conexion))
+                    {
+                        consulta.Fill(tabla);
+                    }//Liberar la consulta
+
+                }//liberar la conexion
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("error en la conexion " + ex.Message);
+            }
+            return tabla;
+        }
+
     }
 }
 
