09cb426270608afc213c67653cdbe6929ac2e23f
Rony <ronal@RONY>
2026-07-01 08:38:21 -0600

    ci

--- pryProyecto/Program.cs
+++ pryProyecto/Program.cs
@@ -12,7 +12,7 @@ namespace pryProyecto
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
-            
+
             using (frmLogin login = new frmLogin())
             {
                 if (login.ShowDialog() == DialogResult.OK)
@@ -20,7 +20,8 @@ namespace pryProyecto
 
                     Application.Run(new frmPrincipal());
                 }
-            //Application.Run(new frmAlumnos());
+                //Application.Run(new frmAlumnos());
+            }
             
         }
     }
